2db67e38cfb51ad8317bfeab944ed232905a308a
Timendainum <james.b.glaser@gmail.com>
2015-02-21 20:51:50 -0600

    added bin

---
+++ bin/codebot.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+rdesktop -g 100% -P -z 10.10.12.25 -u jglaser
---
+++ bin/mountcrypto.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+truecrypt -t --filesystem=ext4 --slot=1 --keyfiles=/run/media/james/LOCKBOX/disk.key /dev/sdc5 /home/james/cryptodisk
+
